3b5eedc56a2f8b65e7c5024a3ef6dae6bd80ad2c
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-08-14 13:30:05 +0545

    customizable options for the plugin

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const slugify = require("slugify");
 const defaultOptions = {
   hLevels: [1, 2, 3, 4, 5, 6],
   setAnchor: false,
-  classNamePrefix: "ulka-header",
   anchorValue: "#",
+  headingClassName: (hLevel) => `ulka-heading-${hLevel}`,
+  anchorClassName: (hLevel) => `ulka-heading-anchors-${hLevel}`,
 };
 
 module.exports = {
@@ -16,15 +17,36 @@ module.exports = {
 
     return {
       plugin: (remarkable) => {
+        const heading_open = remarkable.renderer.rules.heading_open;
+
         remarkable.renderer.rules.heading_open = function (tokens, idx) {
           const hLevel = tokens[idx].hLevel;
-          const className = options.classNamePrefix + hLevel;
-          const id = slugify(tokens[idx + 1].content);
+
+          if (!options.hLevels.includes(hLevel))
+            return heading_open(tokens, idx);
+
+          let headingClassName = "";
+          let anchorClassName = "";
+
+          if (typeof options.headingClassName === "function") {
+            headingClassName = options.headingClassName(hLevel);
+          } else if (typeof options.headingClassName === "string") {
+            headingClassName = options.headingClassName;
+          }
+
+          if (typeof options.anchorClassName === "function") {
+            anchorClassName = options.anchorClassName(hLevel);
+          } else if (typeof options.anchorClassName === "string") {
+            anchorClassName = options.anchorClassName;
+          }
+
+          const headerId = slugify(tokens[idx + 1].content, { lower: true });
+
           const anchor = options.setAnchor
-            ? `<a id="${id}-anchor" href="#${id}">${options.anchorValue}</a>`
+            ? `<a id="${headerId}-anchor" class="${anchorClassName}" href="#${headerId}">${options.anchorValue}</a>`
             : "";
 
-          return `<h${hLevel} class="${className}" id="${id}">${options.prefixHeader}>${anchor}`;
+          return `<h${hLevel} class="${headingClassName}" id="${headerId}">${anchor} `;
         };
       },
     };
